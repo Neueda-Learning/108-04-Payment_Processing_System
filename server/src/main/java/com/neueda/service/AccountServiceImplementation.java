b053da0fda31c402438dae7e4c498532127e4449
Reshma M <22wh1a05b0@gmail.com>
2026-08-01 07:02:14 +0000

    Added service annotations

--- server/src/main/java/com/neueda/service/AccountServiceImplementation.java
+++ server/src/main/java/com/neueda/service/AccountServiceImplementation.java
@@ -5,6 +5,7 @@ import com.neueda.repository.AccountRepository;
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class AccountServiceImplementation implements AccountService {
 
     private final AccountRepository repository;
