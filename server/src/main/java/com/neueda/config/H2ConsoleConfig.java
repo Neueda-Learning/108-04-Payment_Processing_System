413c254630349b6399aeb440c7fa73c8c9c2af1c
Ronak Rewar <2022ucp1729@mnit.ac.in>
2026-08-04 06:14:00 +0000

    fixed controller issue and added config for ui of h2 db

---
+++ server/src/main/java/com/neueda/config/H2ConsoleConfig.java
@@ -0,0 +1,19 @@
+package com.neueda.config;
+
+import org.h2.server.web.JakartaWebServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class H2ConsoleConfig {
+
+    @Bean
+    ServletRegistrationBean<JakartaWebServlet> h2ConsoleServlet() {
+        ServletRegistrationBean<JakartaWebServlet> registration =
+                new ServletRegistrationBean<>(new JakartaWebServlet());
+
+        registration.addUrlMappings("/h2-console/*");
+        return registration;
+    }
+}
